8c71fbfe1f9dbf94570fd1bfc85fe147f2be5fb9
yangjuxu <yangjuxu1_009@qq.com>
2019-10-10 19:40:28 +0800

    reactthunk

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,13 @@ import Counter2 from "./Counter2";
 import App1 from "./App";
 import App2 from "./App2";
 import {hashHistory,Route,Redirect,Switch,HashRouter} from 'react-router-dom';
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
 import appReducer from './reducers';
 
 // 使用合并后的那个Reducer
-const store = createStore(appReducer);
+const store = createStore(appReducer,applyMiddleware(thunk));
 // 2.构建路由
 const Routesa = () => (
         <Provider store={store}>
